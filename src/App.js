1a8426d6a6f592564423e96e5e0f2e1b15a0557c
presh <ogwaraprecious8@gmail.com>
2023-05-01 13:44:06 -0700

    fixed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import './App.css';
+import Starwars from './components/Starwars';
+import React from 'react';
+
+function App() {
+  return (
+   <div>
+    <Starwars />
+   </div>
+  );
+}
+
+export default App;
