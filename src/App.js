c0e0ff5171f31633328963358cf2974fea9a0e7c
presh <ogwaraprecious8@gmail.com>
2023-05-03 00:06:41 -0700

    fixed the react bug

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
-
-import './App.css';
-import Starwars from './components/Starwars';
 import React from 'react';
+import './App.css';
+import Starwars from './component/Starwars';
+
 
 function App() {
   return (
